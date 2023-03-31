c1016d1c4aea4a20a05caaee29f61baa41389387
Matyarg <matias.g15@hotmail.com>
2023-03-31 19:10:39 -0300

    casi terminando

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Link from '@material-ui/core/Link'
 import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
-import { Container } from '@material-ui/core'
+import Container from '@material-ui/core/Container'
 import Avatar from '@material-ui/core/Avatar'
+import Hidden from '@material-ui/core/Hidden'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
+import Divider from '@material-ui/core/divider'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+
 
 const navegationLinks = [
     { name: "About", href:"" },
@@ -28,22 +37,58 @@ const useStyles = makeStyles((theme) => ({
     }
 }))
 
+
+
+
 export default function Header() {
     const styles = useStyles()
+    const [open, setOpen] = useState(false)
+
+
   return (
     <AppBar position='sticky' color='default' >
         <Container maxWidth="md">
             <Toolbar disableGutters>
             <Avatar className={styles.avatar}> M </Avatar>
-            {navegationLinks.map((link) => (
-                <Link 
-                className= {styles.link}
-                color="textPrimary"
-                variant='button'
-                underline='none'
-                href={link.href}
-                > {link.name} 
-                </Link>))}
+            <Hidden xsDown>
+                {navegationLinks.map((link) => (
+                    <Link 
+                        className= {styles.link}
+                        color="textPrimary"
+                        variant='button'
+                        underline='none'
+                        href={link.href}
+                        > {link.name} 
+                    </Link>))}
+            </Hidden>
+            <Hidden smUp>
+                    <IconButton>
+                        <MenuIcon onClick={() => setOpen(true)} />
+                    </IconButton>
+            </Hidden>
+            <SwipeableDrawer anchor="right" open={open} onOpen={true} onClose={false}>
+                <div>
+                    <IconButton>
+                        <ChevronRightIcon onClick={() => setOpen(false)} />
+                    </IconButton>
+                </div>
+                <Divider/>
+                <List>
+                    {navegationLinks.map((link) => (
+                        <ListItem>
+                            <Link 
+                                className= {styles.link}
+                                color="textPrimary"
+                                variant='button'
+                                underline='none'
+                                href={link.href}
+                                > {link.name} 
+                            </Link>
+                        </ListItem>
+                        ))}
+                        
+                </List>
+            </SwipeableDrawer>
             </Toolbar>
         </Container>
     </AppBar>
